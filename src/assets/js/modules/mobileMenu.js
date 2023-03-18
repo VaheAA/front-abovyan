4d283d746360f055cf0abfc6d2d5eddbb12847ae
Vahe Abovyan <vahe.abov@gmail.com>
2023-03-18 13:34:49 +0400

    finish task

--- src/assets/js/modules/mobileMenu.js
+++ src/assets/js/modules/mobileMenu.js
@@ -1,16 +1,20 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const hamburgerBtn = document.querySelector('.hamburger');
 const closeBtn = document.querySelector('.close');
+const body = document.body;
 
 hamburgerBtn.addEventListener('click', () => {
   mobileMenu.classList.add('open');
+  body.classList.add('no-scroll');
 });
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('open');
+  body.classList.remove('no-scroll');
 });
 
 mobileMenu.addEventListener('click', (e) => {
   if (e.target.classList.contains('mobile-menu')) {
     mobileMenu.classList.remove('open');
+    body.classList.remove('no-scroll');
   }
 });
--- src/assets/js/modules/stickyHeader.js
+++ src/assets/js/modules/stickyHeader.js
@@ -6,6 +6,7 @@ const header = document.querySelector('.header');
 const sticky = header.offsetTop;
 
 function myFunction() {
+  console.log(window.pageYOffset);
   if (window.pageYOffset >= 200) {
     header.classList.add('sticky');
   } else {
